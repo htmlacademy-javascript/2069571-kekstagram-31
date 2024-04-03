bc5440df79fd30682ff03287e962ddfcffa94f00
Roman Volkov <volkovroma15@yandex.ru>
2024-04-03 11:58:19 +0300

    подключает файл main.js, который генерирует объекты;  в functions.js добавляет проверку на строку

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,37 @@
 function checkStringLength(string, param) {
-  return string.length <= param;
+  if (string) {
+    return string.length <= param;
+  }
 }
 
 checkStringLength();
 
 function isPalindrom(string) {
-  const newString = string.replaceAll(' ').toLowerCase();
-  let reverseString = '';
+  if (string) {
+    const newString = string.replaceAll(' ').toLowerCase();
+    let reverseString = '';
 
-  for (let i = newString.length - 1; i >= 0; i--) {
-    const letter = newString[i];
-    reverseString += letter;
+    for (let i = newString.length - 1; i >= 0; i--) {
+      const letter = newString[i];
+      reverseString += letter;
+    }
+    return reverseString === newString;
   }
-  return reverseString === newString;
 }
 
 isPalindrom();
 
 function getNumbers(string) {
-  const newString = string.toString();
-  let result = '';
-  for (let i = 0; i <= newString.length; i++) {
-    if (Number.isNaN(parseInt(newString[i], 10)) === false) {
-      result += newString[i];
+  if (string) {
+    const newString = string.toString();
+    let result = '';
+    for (let i = 0; i <= newString.length; i++) {
+      if (Number.isNaN(parseInt(newString[i], 10)) === false) {
+        result += newString[i];
+      }
     }
+    return result === '' ? NaN : Number(result);
   }
-  return result === '' ? NaN : Number(result);
 }
 
 getNumbers();
