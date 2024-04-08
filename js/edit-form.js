9d9a4b5da2ce996cc6b31456bcce10345a3f6f2d
Roman Volkov <volkovroma15@yandex.ru>
2024-04-08 20:17:16 +0300

    добавляет модуль для работы с формой, валидацию полей хэштегов и комментариев

---
+++ js/edit-form.js
@@ -0,0 +1,41 @@
+import { isEscDown } from './util';
+import './form-validation';
+
+const form = document.querySelector('.img-upload__form');
+const uploadInput = form.querySelector('.img-upload__input');
+const closeEditorFormBtn = form.querySelector('.img-upload__cancel');
+const photoEditorForm = form.querySelector('.img-upload__overlay');
+
+const hashtagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text__description');
+
+const onCloseEditorFormBtnClick = () => closeEditorForm();
+
+const onDocumentKeydown = (evt) => {
+  if (isEscDown(evt)) {
+    evt.preventDefault();
+    if (document.activeElement === hashtagField || document.activeElement === commentField) {
+      evt.stopPropagation();
+    } else {
+      closeEditorForm();
+    }
+  }
+};
+
+function openEditorForm() {
+  photoEditorForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  closeEditorFormBtn.addEventListener('click', onCloseEditorFormBtnClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closeEditorForm() {
+  photoEditorForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  document.removeEventListener('click', onCloseEditorFormBtnClick);
+
+  uploadInput.value = '';
+}
+uploadInput.addEventListener('change', openEditorForm);
+
