64a87053130937995d370382a5628def90a4695f
Roman Volkov <volkovroma15@yandex.ru>
2024-04-10 21:15:33 +0300

    добавляет загрузку новой картинки

--- js/editor-form.js
+++ js/editor-form.js
@@ -9,9 +9,12 @@ const form = document.querySelector('.img-upload__form');
 const uploadInput = form.querySelector('.img-upload__input');
 const closeEditorFormBtn = form.querySelector('.img-upload__cancel');
 const photoEditorForm = form.querySelector('.img-upload__overlay');
-
+const preview = form.querySelector('.img-upload__preview > img');
 const hashtagField = form.querySelector('.text__hashtags');
 const commentField = form.querySelector('.text__description');
+const effectsForPreview = form.querySelectorAll('.effects__preview');
+
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const onCloseEditorFormBtnClick = () => closeEditorForm();
 
@@ -31,6 +34,8 @@ function openEditorForm() {
   document.body.classList.add('modal-open');
   closeEditorFormBtn.addEventListener('click', onCloseEditorFormBtnClick);
   document.addEventListener('keydown', onDocumentKeydown);
+
+  onFileInputChange();
 }
 
 function closeEditorForm() {
@@ -45,5 +50,17 @@ function closeEditorForm() {
 
 uploadInput.addEventListener('change', openEditorForm);
 
+function onFileInputChange() {
+  const file = uploadInput.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    const url = URL.createObjectURL(file);
+    preview.src = url;
+    effectsForPreview.forEach((item) => {
+      item.style.backgroundImage = `url(${url})`;
+    });
+  }
+}
 
 export { closeEditorForm };
